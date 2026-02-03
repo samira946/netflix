f75bca17401b410b76112cd9e638bff7011a0841
Alisher <251990@astanait.edu.kz>
2026-02-04 01:56:14 +0500

    ...

--- src/models/Subscriptiondetails.java
+++ src/models/Subscriptiondetails.java
@@ -1,4 +1,19 @@
 package models;
 
 public class Subscriptiondetails {
+    private String planName;
+    private boolean hasAds;
+    private boolean canWatchPremium;
+
+    public Subscriptiondetails(String planName, boolean hasAds, boolean canWatchPremium) {
+        this.planName = planName;
+        this.hasAds = hasAds;
+        this.canWatchPremium = canWatchPremium;
+    }
+
+    public Subscriptiondetails() {}
+
+    public String getPlanName() { return planName; }
+    public boolean isHasAds() { return hasAds; }
+    public boolean isCanWatchPremium() { return canWatchPremium; }
 }
